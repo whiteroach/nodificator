f8fb59cb28a2a33e452adb810e9a1f0ad0fbbb02
whiteroach <pietro.simcic@gmail.com>
2022-12-16 15:41:05 +0100

    updated README

--- README.md
+++ README.md
@@ -3,13 +3,30 @@
 ### **Nodificator has no mercy!**
 
 A script to recursively remove all node_modules throughout a given folder.
+![nodificator](https://i.ibb.co/PN5vd42/nodificator.png)
+
+### Usage:
+
+Choose a directory that you want to free from the bourdon of node_modules and run :
+
+```
+cargo run <path>
+```
+
+You can also type "h" or "help" to access the commands available:
+
+```
+cargo run h
+//or
+cargo run help
+```
 
 <br>
 
 Todo:
 
 - [ ] Improve README file
-- [ ] Re-Organize modules
-- [ ] Improve Error Handling
-- [ ] Add Help functionality
+- [x] Re-Organize modules
+- [x] Improve Error Handling
+- [x] Add Help functionality
 - [x] Add a counter that counts number of node_modules removed recursively.
--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,5 @@
-use std::env;
-use std::path::PathBuf;
-use text_colorizer::*;
 mod module_killer;
 
 fn main() -> std::io::Result<()> {
-    let args: Vec<String> = env::args().skip(1).collect();
-    let file_path = parse_args(args);
-    let path = file_path.clone();
-    let mut counter: u32 = 0;
-    match module_killer::kill_module(file_path, &mut counter) {
-        Ok(_) => {
-            eprintln!(
-                "\u{1F977}  {} node_modules recursively removed  throughout  {:?}",
-                counter, path
-            )
-        }
-        Err(e) => {
-            eprintln!("{}: {}", "Error:".red().bold(), e);
-            std::process::exit(1);
-        }
-    }
-    Ok(())
-}
-
-fn parse_args(args: Vec<String>) -> PathBuf {
-    match args {
-        args if args.is_empty() => {
-            eprintln!(
-                "{} Path argument is required ex:\n cargo run <../path>\n",
-                "Error:".red().bold()
-            );
-            std::process::exit(1)
-        },
-        args if args.len() > 1 => {
-            eprintln!(
-                "{} Nodificator require only one parameter path, ex: cargo run <path/>",
-                "Error:".red().bold()
-            );
-            std::process::exit(1)
-        },
-        args if args[0] == "help" || args[0] == "h" => {
-            call_help();
-            std::process::exit(1)
-        },
-        _ => PathBuf::from(args[0].clone()),
-    }
-}
-
-fn call_help() -> () {
-    eprintln!("NODIFICATOR:\nA script to recursively remove all node_modules throughout a given folder.\nUsage: cargo run <../path>\n   h, help   This help text ")
+    module_killer::run()
 }
