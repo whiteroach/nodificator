85ba935c3a683323f9098ae8fb2f0bd5a8e856a2
whiteroach <pietro.simcic@gmail.com>
2022-12-16 15:04:11 +0100

    refactor error handling of kill_module

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,52 @@
 use std::env;
+use std::path::PathBuf;
 use text_colorizer::*;
 mod module_killer;
 
 fn main() -> std::io::Result<()> {
     let args: Vec<String> = env::args().skip(1).collect();
     let file_path = parse_args(args);
-    let mut counter = 0;
-    match module_killer::kill_module(file_path.to_string(), &mut counter) {
-        Ok(count) => {
+    let path = file_path.clone();
+    let mut counter: u32 = 0;
+    match module_killer::kill_module(file_path, &mut counter) {
+        Ok(_) => {
             eprintln!(
-                "\u{1F977}  {} node_modules recursively removed  throughout  {}",
-                count, file_path
+                "\u{1F977}  {} node_modules recursively removed  throughout  {:?}",
+                counter, path
             )
         }
-        Err(_) => {
-            eprintln!("{}", "Error:".red().bold());
+        Err(e) => {
+            eprintln!("{}: {}", "Error:".red().bold(), e);
             std::process::exit(1);
         }
     }
-
     Ok(())
 }
 
-fn parse_args(args: Vec<String>) -> String {
+fn parse_args(args: Vec<String>) -> PathBuf {
     match args {
         args if args.is_empty() => {
             eprintln!(
-                "{} path argument is required ex: cargo run <path/>",
+                "{} Path argument is required ex:\n cargo run <../path>\n",
                 "Error:".red().bold()
             );
             std::process::exit(1)
-        }
+        },
         args if args.len() > 1 => {
             eprintln!(
                 "{} Nodificator require only one parameter path, ex: cargo run <path/>",
                 "Error:".red().bold()
             );
             std::process::exit(1)
-        }
-        _ => args[0].clone(),
+        },
+        args if args[0] == "help" || args[0] == "h" => {
+            call_help();
+            std::process::exit(1)
+        },
+        _ => PathBuf::from(args[0].clone()),
     }
+}
+
+fn call_help() -> () {
+    eprintln!("NODIFICATOR:\nA script to recursively remove all node_modules throughout a given folder.\nUsage: cargo run <../path>\n   h, help   This help text ")
 }
--- src/module_killer.rs
+++ src/module_killer.rs
@@ -1,31 +1,19 @@
 use std::fs;
-use text_colorizer::*;
+use std::path::PathBuf;
 
-pub fn kill_module(file_path: String, count: &mut u32) -> Result<u32, std::io::Error> {
-    let metadata = fs::metadata(&file_path)?.file_type();
+pub fn kill_module(path: PathBuf, count: &mut u32) -> Result<(), std::io::Error> {
+    let metadata = fs::metadata(&path)?.file_type();
     if metadata.is_dir() {
-        for entry in fs::read_dir(&file_path)? {
+        for entry in fs::read_dir(&path)? {
             let dir = entry?;
             if dir.file_name() == "node_modules" {
+                // fs::remove_dir_all(dir.path())?;
                 *count += 1;
-                eprintln!("removed {}/node_modules", &file_path.green().bold(),);
-
-                // match fs::remove_dir_all(file_path.to_string() + "/node_modules") {
-                //     Ok(_) =>  eprintln!("removed"),
-                //     Err(e) => eprintln!("{} cannot remove: {e}", "Error:".red().bold())
-                // }//map the error
+                eprintln!("removed {:?}", dir.path());
             } else {
-                if let Some(file_name) = dir.file_name().to_str() {
-                    match kill_module(file_path.to_string() + "/" + file_name, count) {
-                        Ok(counter) => counter,
-                        Err(e) => {
-                            eprintln!("{} cannot remove: {e}", "Error:".red().bold());
-                            std::process::exit(1)
-                        }
-                    };
-                }
+                kill_module(dir.path(), count)?;
             }
         }
     }
-    Ok(*count)
+    Ok(())
 }
